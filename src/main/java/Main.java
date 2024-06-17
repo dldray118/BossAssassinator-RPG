4176095f89eb47b5489c2ec4681308c82643adc0
Damion Dray <dldray@asu.edu>
2024-06-17 14:06:57 -0700

    initial commit of Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+//package com.game.engine;
+
+/**
+ * Main class to start the Boss Assassinator RPG Game.
+ */
+public class Main {
+
+        public static void main(String[] args) {
+
+            System.out.println("Coming soon: The Boss Assassinator!");
+        }
+}
