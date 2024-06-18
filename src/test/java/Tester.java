4578c25f4140503c5a3f0b70525e21e26e18c8ab
Damion Dray <dldray@asu.edu>
2024-06-18 10:39:17 -0700

    Created Tester class and update build.gradle

---
+++ src/test/java/Tester.java
@@ -0,0 +1,12 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Tester {
+
+    @Test
+    public void testMain() {
+        // This is a placeholder test to show JUnit is working
+        assertTrue(true);
+    }
+}
