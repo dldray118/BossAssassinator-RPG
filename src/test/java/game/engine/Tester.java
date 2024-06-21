0e5b6cdfcd0bbddf829bebbf99dede0d214c38a7
Damion Dray <dldray@asu.edu>
2024-06-21 15:35:15 -0700

    Modified directory

---
+++ src/test/java/game/engine/Tester.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tester {
+
+    @Test
+    public void testMain() {
+        // This is a placeholder test to show JUnit is working
+        assertTrue(true);
+    }
+}
