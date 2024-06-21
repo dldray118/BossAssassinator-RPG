165fb1eead2ba5cac77e9bb77f649d2848c2d759
Damion Dray <dldray@asu.edu>
2024-06-21 15:57:40 -0700

    Fixed directory

---
+++ src/main/java/engine/Main.java
@@ -0,0 +1,12 @@
+package game.engine;
+
+
+
+/**
+ * Main class to start the Street Fighter RPG Game.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Coming soon: The Boss Assassinator!");
+    }
+}
