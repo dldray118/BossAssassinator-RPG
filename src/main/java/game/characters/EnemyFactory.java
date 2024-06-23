99caafec29558c2caf8b388a80afe3a3a24da8dd
Damion Dray <dldray@asu.edu>
2024-06-22 19:44:44 -0700

    Initial commit EnemyFactory

---
+++ src/main/java/game/characters/EnemyFactory.java
@@ -0,0 +1,19 @@
+package game.characters;
+
+/**
+ * EnemyFactory class for creating different types of enemies.
+ */
+class EnemyFactory {
+    public Enemy createEnemy(String type) {
+        switch (type.toLowerCase()) {
+            case "small":
+                return new SmallEnemy();
+            case "medium":
+                return new MediumEnemy();
+            case "boss":
+                return new BossEnemy();
+            default:
+                throw new IllegalArgumentException("Unknown enemy type: " + type);
+        }
+    }
+}
