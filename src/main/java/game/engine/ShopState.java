2706d678a14371c6c73f546211d0d1c9f963d6e7
Damion Dray <dldray@asu.edu>
2024-06-22 18:48:04 -0700

    Initial commit ShopState

---
+++ src/main/java/game/engine/ShopState.java
@@ -0,0 +1,17 @@
+package game.engine;
+
+/**
+ * ShopState class handles the shop phase of the game.
+ */
+class ShopState implements GameState {
+    private GameContext context;
+
+    public ShopState(GameContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public void handle() {
+        
+    }
+}
