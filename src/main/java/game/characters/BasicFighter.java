767a1e2df2d0d3e3a1c55f1da3e3cb8dae39fea9
Damion Dray <dldray@asu.edu>
2024-06-22 19:11:44 -0700

    Initial commit BasicFighter

---
+++ src/main/java/game/characters/BasicFighter.java
@@ -0,0 +1,30 @@
+package game.characters;
+
+/**
+ *
+ * BasicFighter class implements the Fighter interface with basic capabilities.
+ */
+class BasicFighter implements Fighter {
+    private String name;
+    private String skills;
+
+    public BasicFighter(String name) {
+        this.name = name;
+        this.skills = "Punch Attack";
+    }
+
+    @Override
+    public void attack() {
+        System.out.println(name + " uses " + skills);
+    }
+
+    @Override
+    public void addSkill(String skill) {
+        this.skills += ", " + skill;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
