baac76eb287d2fa730764687a8d7c565b03d243b
Damion Dray <dldray@asu.edu>
2024-06-22 17:57:50 -0700

    Add src/main

---
+++ src/main/java/game/engine/Main.java
@@ -0,0 +1,12 @@
+package game.engine;
+
+import java.util.Scanner;
+
+/**
+ * Main class to start the Street Fighter RPG Game.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Coming soon: The Boss Assassinator!");
+    }
+}
