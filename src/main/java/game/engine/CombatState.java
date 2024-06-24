b2bedc056e19ac2203ce47ef1556c05db12c264c
Damion Dray <dldray@asu.edu>
2024-06-23 18:01:50 -0700

    Enter print statements for combat

--- src/main/java/game/engine/CombatState.java
+++ src/main/java/game/engine/CombatState.java
@@ -1,5 +1,8 @@
 package game.engine;
 
+import game.characters.Enemy;
+import game.characters.EnemyFactory;
+
 /**
  * CombatState class handles the combat phase of the game.
  */
@@ -12,5 +15,10 @@ class CombatState implements GameState {
 
     @Override
     public void handle() {
+        System.out.println("Entering combat...");
+        EnemyFactory enemyFactory = new EnemyFactory();
+        Enemy enemy = enemyFactory.createEnemy("medium");
+        System.out.println("Combat finished.");                             // Simulate combat here.
+        context.setState(new ExploringState(context));
     }
 }
